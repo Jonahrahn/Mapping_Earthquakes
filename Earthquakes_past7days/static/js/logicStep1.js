7e44cee2878750b370866d3ba4b1bde91d4165d1
JonahRahn <jonah.rahn\@gmail.com>
2022-02-09 11:38:56 -0800

    Updated to show past 7 days of Earthquakes

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -29,7 +29,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -37,19 +37,29 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Streets: streets,
-    Dark: dark
+    "Streets": streets,
+    "Satellite": satelliteStreets
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
-})
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+
+
+
 
 //  // Grabbing our GeoJSON data.
 //  L.geoJSON(sanFranAirport, {
@@ -65,24 +75,24 @@ let myStyle = {
     weight: 2
 }
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/Jonahrahn/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+// // Accessing the Toronto neighborhoods GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/Jonahrahn/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Accessing the airport GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/Jonahrahn/Mapping_Earthquakes/main/torontoRoutes.json";
+// // Accessing the airport GeoJSON URL
+// let torontoData = "https://raw.githubusercontent.com/Jonahrahn/Mapping_Earthquakes/main/torontoRoutes.json";
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map);
-    style:myStyle,
-    // onEachFeature: function(feature, layer) {
-    //     layer.bindPopup("<h3> Airline" + feature.properties.airline  + "</h3> <hr><h3> Destination: "
-    //      + feature.properties.dst "</h3> ");
-    // }
-// }).addTo(map);
-});
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data).addTo(map);
+//     style:myStyle,
+//     // onEachFeature: function(feature, layer) {
+//     //     layer.bindPopup("<h3> Airline" + feature.properties.airline  + "</h3> <hr><h3> Destination: "
+//     //      + feature.properties.dst "</h3> ");
+//     // }
+// // }).addTo(map);
+// });
 
 // // Get data from cities.js
 // let cityData = cities;
